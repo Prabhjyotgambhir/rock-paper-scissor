f9936b02b860dcd9626aedcaa6d37826e486667f
prabhjyotgambhir <prabhjyotgambhir1994@gmail.com>
2017-09-15 01:01:11 +0530

    js logic

--- app.js
+++ app.js
@@ -3,9 +3,18 @@
 	
 var score = 0;
 var playerChoice;
+var store;
 
 var readable = {
 	'0' : 'Rock',
 	'1' : 'Paper',
 	'2' : 'Scissor'
 }
+
+var cpuChoice =  function() {
+ 		store = Math.floor(Math.random() * 3);
+ 		text = readable(store);
+}
+
+console.log(cpuChoice, text);
+
